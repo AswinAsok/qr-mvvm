5cfee55277de7d6f67a393a2eeb9d7262f6a1a08
Aswin Asok <aswinasokofficial@gmail.com>
2025-02-06 17:51:29 +0530

    feat(scanResult):added-widget

--- lib/pages/qrscanner/views/scanner_view.dart
+++ lib/pages/qrscanner/views/scanner_view.dart
@@ -20,115 +20,220 @@ class ScannerView extends StatelessWidget {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  Scanner(viewModel: viewModel),
                   Container(
-                    width: double.infinity,
-                    height: 425,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
                       children: [
-                        Expanded(
-                          child: Center(
-                            child: Container(
-                              width: 200,
-                              height: 200,
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: Colors.transparent,
-                                  width: 3,
-                                ),
-                              ),
-                              child: GestureDetector(
-                                onTap: () {
-                                  viewModel.toggleScannerVisibility();
-                                },
-                                child: Stack(
-                                  children: [
-                                    QRScannerCorner(),
-                                    Positioned(
-                                      right: 0,
-                                      child: Transform.rotate(
-                                        angle: Math.pi / 2,
-                                        child: QRScannerCorner(),
-                                      ),
-                                    ),
-                                    Positioned(
-                                      right: 0,
-                                      bottom: 0,
-                                      child: Transform.rotate(
-                                        angle: Math.pi,
-                                        child: QRScannerCorner(),
-                                      ),
-                                    ),
-                                    Positioned(
-                                      bottom: 0,
-                                      child: Transform.rotate(
-                                        angle: -Math.pi / 2,
-                                        child: QRScannerCorner(),
-                                      ),
-                                    ),
-                                    Center(
-                                      child: viewModel.isScannerVisible
-                                          ? SizedBox(
-                                              width: 175,
-                                              height: 175,
-                                              child: ClipRRect(
-                                                borderRadius:
-                                                    BorderRadius.circular(10),
-                                                child: MobileScanner(
-                                                  onDetect: (capture) {},
-                                                ),
-                                              ),
-                                            )
-                                          : Column(
-                                              mainAxisSize: MainAxisSize.min,
-                                              children: [
-                                                Icon(
-                                                  Icons.touch_app,
-                                                  color: Colors.black,
-                                                  size: 40,
-                                                ),
-                                                SizedBox(height: 8),
-                                                Text(
-                                                  "Tap to Scan",
-                                                  style: TextStyle(
-                                                    color: Colors.black,
-                                                    fontSize: 16,
-                                                    fontWeight: FontWeight.w500,
-                                                  ),
-                                                ),
-                                              ],
-                                            ),
-                                    ),
-                                  ],
-                                ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Scan Result',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                          ),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.pop(context);
+                              },
+                              child: Icon(
+                                Icons.close,
+                                color: Colors.black,
+                                size: 24,
+                              ),
+                            )
+                          ],
                         ),
-                        Center(
+                        SizedBox(height: 10),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
                           child: Container(
-                            padding: EdgeInsets.all(20),
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 15),
+                            decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(10),
+                            ),
                             child: Text(
-                              "Place a QR code inside the frame to scan it",
+                              "Txt",
                               style: TextStyle(
                                 fontSize: 16,
-                                color: Colors.grey,
                               ),
                             ),
                           ),
                         ),
+                        SizedBox(height: 20),
+                        SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              ElevatedButton.icon(
+                                style: ElevatedButton.styleFrom(
+                                  foregroundColor: Colors.white,
+                                  backgroundColor: Colors.black54,
+                                ),
+                                onPressed: () {},
+                                icon: Icon(Icons.copy, color: Colors.white),
+                                label: Text("Copy",
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                              SizedBox(height: 10),
+                              ElevatedButton.icon(
+                                style: ElevatedButton.styleFrom(
+                                  foregroundColor: Colors.white,
+                                  backgroundColor: Colors.black54,
+                                ),
+                                onPressed: () async {},
+                                icon: Icon(Icons.open_in_browser,
+                                    color: Colors.white),
+                                label: Text("Open in Browser",
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                              SizedBox(height: 10),
+                              ElevatedButton.icon(
+                                style: ElevatedButton.styleFrom(
+                                  foregroundColor: Colors.white,
+                                  backgroundColor: Colors.black54,
+                                ),
+                                onPressed: () {
+                                  print("Share data");
+                                },
+                                icon: Icon(Icons.share, color: Colors.white),
+                                label: Text("Share",
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                            ],
+                          ),
+                        )
                       ],
                     ),
-                  ),
+                  )
                 ],
               )
             ],
           ),
         ));
       }),
+    );
+  }
+}
+
+class Scanner extends StatelessWidget {
+  const Scanner({
+    super.key,
+    required this.viewModel,
+  });
+
+  final ScannerViewModel viewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 425,
+      decoration: BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Center(
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.transparent,
+                    width: 3,
+                  ),
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                    viewModel.toggleScannerVisibility();
+                  },
+                  child: Stack(
+                    children: [
+                      QRScannerCorner(),
+                      Positioned(
+                        right: 0,
+                        child: Transform.rotate(
+                          angle: Math.pi / 2,
+                          child: QRScannerCorner(),
+                        ),
+                      ),
+                      Positioned(
+                        right: 0,
+                        bottom: 0,
+                        child: Transform.rotate(
+                          angle: Math.pi,
+                          child: QRScannerCorner(),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        child: Transform.rotate(
+                          angle: -Math.pi / 2,
+                          child: QRScannerCorner(),
+                        ),
+                      ),
+                      Center(
+                        child: viewModel.isScannerVisible
+                            ? SizedBox(
+                                width: 175,
+                                height: 175,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: MobileScanner(
+                                    onDetect: (capture) {},
+                                  ),
+                                ),
+                              )
+                            : Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Icon(
+                                    Icons.touch_app,
+                                    color: Colors.black,
+                                    size: 40,
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    "Tap to Scan",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                "Place a QR code inside the frame to scan it",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
